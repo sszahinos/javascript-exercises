89fd0fd6c0ec188a4ce8c09ad5de7451a88094a0
sszahinos <sergiosaza6@gmail.com>
2021-10-21 18:09:06 +0200

    Ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    fibo = 1;
+    lastNum = 0;
+    aux = 0;
 
+    if (num < 0) {
+        return 'OOPS';
+    }
+
+    for (let index = 1; index < num; index++) {
+        aux = fibo;
+        fibo += lastNum;
+        lastNum = aux;
+    }
+
+    return fibo;
 };
 
 // Do not edit below this line

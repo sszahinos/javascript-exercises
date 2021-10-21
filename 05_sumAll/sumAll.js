d00ec51f67bc43a6c76dda8ad8201a2e964f0fdc
sszahinos <sergiosaza6@gmail.com>
2021-10-21 12:46:02 +0200

    Ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let aux = 0;
 
+    if (num1 < 0 || num2 < 0 
+        || typeof(num1) !== 'number' 
+        || typeof(num2) !== 'number') {
+            return "ERROR";
+        }
+
+    if (num1 > num2) {
+        aux = num1;
+        num1 = num2;
+        num2 = aux;
+    }
+
+    for (let index = num1; index <= num2; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

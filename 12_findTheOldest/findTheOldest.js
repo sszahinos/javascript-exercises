49f627692edbb172dc860d3fef288c66063ea94f
sszahinos <sergiosaza6@gmail.com>
2021-10-21 18:28:53 +0200

    Ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const CURRENT_DATE = new Date();
+    
+    let oldestPerson = null;
+    let yearsRecord = 0;
+    let years = 0;
+    
 
+    people.forEach(person => {
+        if (person.hasOwnProperty("yearOfDeath")) {
+            years = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            years = CURRENT_DATE.getFullYear() - person.yearOfBirth;
+        }
+
+        if (yearsRecord < years) {
+            oldestPerson = person;
+            yearsRecord = years;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line

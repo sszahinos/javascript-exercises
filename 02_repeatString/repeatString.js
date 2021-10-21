38404fa5e7699707599f16adeb36f9850e03cfc0
sszahinos <sergiosaza6@gmail.com>
2021-10-21 11:48:10 +0200

    Ex 0 and 1 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, iterations) {
+    repeated = word;
+    
+    if (iterations === 0) {
+        return "";
+    } else if (iterations < 0) {
+        return "ERROR";
+    }
 
+    for (let index = 0; index < iterations - 1; index++) {
+        repeated += word;
+    }
+
+    return repeated;
 };
 
 // Do not edit below this line

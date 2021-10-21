957139621044b0bb96897288098b4817b946e45e
sszahinos <sergiosaza6@gmail.com>
2021-10-21 11:53:52 +0200

    Ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    characters = str.split("");
+    let phrase = "";
 
+    for (let index = characters.length - 1; index >= 0; index--) {
+        phrase += characters[index];
+    }
+
+    return phrase;
 };
 
 // Do not edit below this line

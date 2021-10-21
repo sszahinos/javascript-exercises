da127ba3b88500ce51616dd8756d7fa974322fb5
sszahinos <sergiosaza6@gmail.com>
2021-10-21 12:28:37 +0200

    Ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let index = -1;
 
+    args.forEach( (arg) => {
+        index = arr.indexOf(arg);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        index = -1;
+    });
+
+    return arr;
 };
 
 // Do not edit below this line

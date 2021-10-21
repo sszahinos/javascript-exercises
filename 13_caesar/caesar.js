980788157d96bd5e4270e92604fb2eb1946bbdbd
sszahinos <sergiosaza6@gmail.com>
2021-10-21 20:21:42 +0200

    Ex 13 started but not finished

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,27 @@
-const caesar = function() {
+const ALPHABET_LOW = "abcdefghijklmnopqrstuvwxyz",
+    ALPHABET_UPP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+const caesar = function(text, shift) {
+
+
+    for (let index = 0; index < text.length; index++) {
+        if (ALPHABET_LOW.includes(text.charAt(index))) {
+            ALPHABET_LOW.indexOf(text.charAt(index))
+            text = text.charAt(index).replace(ALPHABET_LOW.charAt(index + shift));
+        
+        } else if (ALPHABET_UPP.includes(text.charAt(index))) {
+            ALPHABET_UPP.indexOf(text.charAt(index)) + shift;
+            text = text.charAt(index).replace(ALPHABET_UPP.charAt(index + shift));
+        }
+        
+    }
+
+    return text;
 };
+
+function findNewLetter(old, shift) {
+
+}
 
 // Do not edit below this line
 module.exports = caesar;

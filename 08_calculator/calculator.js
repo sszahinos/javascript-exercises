e51ee086e7441cddb4e25b775eb8c417710021bf
sszahinos <sergiosaza6@gmail.com>
2021-10-21 15:17:13 +0200

    ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+  args.forEach(arg => {
+    sum += arg;
+  });
+
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+
+  numbers.forEach(num => {
+    sum += num;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let total = 1;
 
+  if (nums.length === 0) {
+    total = 0;
+  }
+
+  nums.forEach(num => {
+    total *= num;
+  });
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let total = 1;
+
+  for (let index = 0; index < num2; index++) {
+    total *= num1;
+  }
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+
+  for (let index = num; index > 0; index--) {
+    total *= index;
+  }
+
+  return total;
+
 };
 
 // Do not edit below this line
